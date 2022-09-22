e600837194d2355cc4653ca96494834e0a5c40bf
Subin Lee <80376756+dltnqls9788@users.noreply.github.com>
2022-09-22 22:53:03 +0900

    Time: 1394 ms (13.71%), Space: 0B (100.00%) - LeetHub

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -1,11 +1,17 @@
 # Please write a DELETE statement and DO NOT write a SELECT statement.
 # Write your MySQL query statement below
-DELETE
-FROM person
-WHERE Id NOT IN (
-SELECT sub.min_id
-FROM (
-SELECT Email, MIN(Id) AS min_id
-FROM Person
-GROUP BY Email 
-) sub)
+# DELETE
+# FROM person
+# WHERE Id NOT IN (
+# SELECT sub.min_id
+# FROM (
+# SELECT Email, MIN(Id) AS min_id
+# FROM Person
+# GROUP BY Email 
+# ) sub)
+
+
+DELETE p1
+FROM Person p1
+     INNER JOIN Person p2 ON p1.Email = p2.Email
+WHERE p1.ID > p2.Id
