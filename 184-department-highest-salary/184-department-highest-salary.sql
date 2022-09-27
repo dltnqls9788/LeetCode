3b8fe42c59f81ac162ad24c2cae0774ddb1bb3a4
Subin Lee <80376756+dltnqls9788@users.noreply.github.com>
2022-09-27 13:30:04 +0900

    Time: 565 ms (86.74%), Space: 0B (100.00%) - LeetHub

--- 184-department-highest-salary/184-department-highest-salary.sql
+++ 184-department-highest-salary/184-department-highest-salary.sql
@@ -1,13 +1,15 @@
 # Write your MySQL query statement below
 
-SELECT d.name AS department
-     , e.name AS employee
-     , e.salary
+SELECT d.name AS Department
+      ,e.name AS Employee
+      ,e.salary
 FROM employee AS e
-     INNER JOIN(
-     SELECT departmentid, MAX(salary) AS max_salary
-     FROM employee
-     GROUP BY departmentid
-     ) AS dh ON e.departmentid = dh.departmentid
-             AND e.salary = dh.max_salary
-     INNER JOIN department AS d ON d.id = e.departmentid
+     INNER JOIN (
+        -- 부서에서 가장 많이 벌 때에 그 임금과 부서 id --
+        SELECT departmentID, MAX(salary) AS max_salary
+        FROM employee
+        GROUP BY departmentID 
+        ) AS dh ON e.departmentID = dh.departmentID
+                AND e.salary = dh.max_salary
+                
+     INNER JOIN department AS d ON d.id = e.departmentID
